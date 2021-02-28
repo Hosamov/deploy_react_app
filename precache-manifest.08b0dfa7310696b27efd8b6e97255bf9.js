d9ba8f8b6219efb917131fffb94df753bb968df5
Hosamov <hosamov@hotmail.com>
2021-02-28 07:01:39 -0800

    Updates

---
+++ precache-manifest.08b0dfa7310696b27efd8b6e97255bf9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbbd551d1337b4062ab7",
+    "url": "/deploy_react_app/static/css/main.4743816a.chunk.css"
+  },
+  {
+    "revision": "bbbd551d1337b4062ab7",
+    "url": "/deploy_react_app/static/js/main.bbbd551d.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/deploy_react_app/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "981ecdae9cfefd6b33b1",
+    "url": "/deploy_react_app/static/js/runtime~main.981ecdae.js"
+  },
+  {
+    "revision": "6e942143aa5a0017f697c5735380ebd1",
+    "url": "/deploy_react_app/index.html"
+  }
+];
